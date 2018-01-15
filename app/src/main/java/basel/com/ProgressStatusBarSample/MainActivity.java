2dcd421175e193dd5e74a429e1d11ca92a8d89ea
BaselHorany <30597730+BaselHorany@users.noreply.github.com>
2018-01-15 17:51:59 +0300

    Update MainActivity.java

--- app/src/main/java/basel/com/ProgressStatusBarSample/MainActivity.java
+++ app/src/main/java/basel/com/ProgressStatusBarSample/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         wait.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mProgressStatusBar.setWaiting(6000);
+                mProgressStatusBar.setWaitting(6000);
             }
         });
 
